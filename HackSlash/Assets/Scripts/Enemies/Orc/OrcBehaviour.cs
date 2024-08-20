f74fa26b6c1635301509d0f50ca9e619de1aca15
Sunnatullo <sunnatullo.jj@gmail.com>
2024-08-20 14:13:24 +0500

    BeforeBuildSettingsChange

--- HackSlash/Assets/Scripts/Enemies/Orc/OrcBehaviour.cs
+++ HackSlash/Assets/Scripts/Enemies/Orc/OrcBehaviour.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Enemies.Orc
@@ -13,22 +15,69 @@ namespace Enemies.Orc
         [SerializeField] private float _groundCheckRadius;
         [SerializeField] private LayerMask _groundLayer;
 
-        [SerializeField] private float _upValue;
+        private BoxCollider2D _myColliderr;
+
         // OVERRIDES.
         protected override void Start()
         {
+            _myColliderr = GetComponent<BoxCollider2D>();
             _orcAttack = GetComponent<OrcAttack>();
             SFXManager.Instance.PlaySFX(SFXType.OrcSpawn);
-
-            MoveUp(_upValue);
             base.Start();
+            FirstJump();
         }
 
-        private void MoveUp(float yAxis)
+
+        private void SetSortingLayerToDefault()
         {
-            transform.position = new Vector2(transform.position.x, transform.position.y + yAxis);
-            Debug.Log("Changed!");
+            // Get all SpriteRenderer components in this GameObject and its children
+            SpriteRenderer[] renderers = GetComponentsInChildren<SpriteRenderer>();
+
+            // Loop through each SpriteRenderer and set the sorting layer to "Default"
+            foreach (SpriteRenderer spriteRenderer in renderers)
+            {
+                spriteRenderer.sortingLayerName = "Default"; // Set this to your desired sorting layer
+            }
         }
+
+
+        private List<Vector2> _spawnPositions = new List<Vector2>
+        {
+            new (-50f, -30f),
+            new (20f, -30f),
+            new (-15f, -30f),
+            new (60f, -30f)
+        };
+
+        [SerializeField] private float _firstForceValue = 20f;
+        [SerializeField] private float _colliderEnableTime = 1f;
+
+        private void FirstJump()
+        {
+            _myColliderr.enabled = false;
+            ChangePositionTo(GetRandomVector(_spawnPositions));
+            float chosenForceValue = (Random.value > 0.5f) ? _firstForceValue : _firstForceValue * 1.2f;
+            _rb.AddForce(Vector2.up * chosenForceValue);
+            StartCoroutine(EnableColliderAfterTime(_myColliderr, 1f));
+        }
+
+        private Vector2 GetRandomVector(List<Vector2> list)
+        {
+            return list[Random.Range(0, list.Count)];
+        }
+
+        private IEnumerator EnableColliderAfterTime(Collider2D collider, float time)
+        {
+            yield return new WaitForSeconds(time);
+            collider.enabled = true;
+            SetSortingLayerToDefault();
+        }
+
+        private void ChangePositionTo(Vector2 pos)
+        {
+            transform.position = pos;
+        }
+
 
         protected override void MoveTowardsPlayer()
         {
